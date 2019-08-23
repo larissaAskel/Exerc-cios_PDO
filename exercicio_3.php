fdf63439d02fec35fc112cb6a50892bfed07560a
larissaAskel <48127879+larissaAskel@users.noreply.github.com>
2019-08-23 11:32:43 -0300

    Create exercicio_3.php

---
+++ exercicio_3.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <title>Formulário</title>
+    </head>
+
+    <body>
+    <form action="process_form_3.php" method="post">
+    <table>
+    <tr>
+    <td><p><strong>Fabricante:</strong></p></td>
+    <td><input type="text" name="fabricante" size="30" maxlength="30" /></td>
+    </tr>
+    <tr>
+    <td><p><strong>Ano de Fabricação:</strong></p></td>
+    <td><input type="text" name="ano_fabricacao" size="40"/></td>
+    </tr>
+    <td><p><strong>quilometragem:</strong></p></td>
+    <td><input type="text" name="quilometragem" size="40"/></td>
+    </tr>
+    <td><input type="submit" name="submit" value="Enviar" /></td>
+    </tr>
+    </table>
+    </form>
+    </body>
+</html>
