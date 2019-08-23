0a5e3e36b54491e9c13531a071f3300e50d7e68c
larissaAskel <48127879+larissaAskel@users.noreply.github.com>
2019-08-23 11:31:55 -0300

    Create exercicio_2.php

---
+++ exercicio_2.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <title>Formulário</title>
+    </head>
+
+    <body>
+    <form action="process_form_2.php" method="post">
+    <table>
+    <tr>
+    <td><p><strong>Nome:</strong></p></td>
+    <td><input type="text" name="name" size="30" maxlength="30" /></td>
+    </tr>
+    <tr>
+    <td><p><strong>Idade:</strong></p></td>
+    <td><input type="text" name="age" size="40"/></td>
+    </tr>
+    <td><input type="submit" name="submit" value="Enviar" /></td>
+    </tr>
+    </table>
+    </form>
+    </body>
+</html>
